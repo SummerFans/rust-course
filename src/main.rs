ae0486c95df05d5b45f8630d91bdc1bef9174d91
summer <x.xiaokang@gmail.com>
2021-11-09 13:38:22 +0800

    initial comment

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
 fn main() {
-    println!("Hello, world!");
+    // TODO 枚举
+    /*
+     *
+     */
+    #[derive(Debug)]
+    enum IpAddKind {
+        V4,
+        V6,
+    }
+    let four = IpAddKind::V4;
+    let size = IpAddKind::V6;
+    println!("Hello, world! {:?}", four);
+    println!("Hello, world! {:?}", size);
+
+    #[derive(Debug)]
+    struct IpAddr {
+        kind: IpAddKind,
+        address: String,
+    }
+
+    let home = IpAddr {
+        kind: IpAddKind::V4,
+        address: String::from("10.0.0.10"),
+    };
+    println!("{:#?}", home);
+
+    #[derive(Debug)]
+    enum IpAddKindVal {
+        V4(u8, u8, u8, u8),
+        V6(String),
+    }
+
+    let ip_v4_addr = IpAddKindVal::V4(10, 10, 10, 10);
+    let ip_v6_addr = IpAddKindVal::V6(String::from("::1"));
+    println!("Hello, world! {:#?}", ip_v4_addr);
+    println!("Hello, world! {:#?}", ip_v6_addr);
+
+    println!("---[给枚举定义方法]--------------------------------");
+
+    impl IpAddKindVal {
+        fn connect(&self) -> bool {
+            true
+        }
+    }
+    if IpAddKindVal::V4(10, 10, 10, 10).connect() {
+        println!("is connect");
+    }
+
 }
